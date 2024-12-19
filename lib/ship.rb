5145f78f3baa280a6b682923061dc284358b951c
Jacob Inman <jrinman6295@gmail.com>
2024-12-19 13:38:04 -0800

    ship initialized complete

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,3 +1,12 @@
 class Ship
+    attr_reader :name, :health, :length, :sunk
+
+    def initialize(name, health)
+        @name = name
+        @health = health
+        @length = health
+        @sunk = false
+    end
+
 
 end
