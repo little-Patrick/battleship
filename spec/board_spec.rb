6e32925553be3a88299dc838dde91e3a1144cfc6
little-Patrick <Little.patrick@outlook.com>
2024-12-20 14:46:40 -0700

    board initialized

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,30 +4,15 @@ require './spec/spec_helper'
 RSpec.describe Board do
   before(:each) do
     @board = Board.new
+   
   end
 
   it 'exists' do 
     expect(@board).to be_an_instance_of(Board)
   end
 
-  it 'has a board' do
-    expect(@board.cells).to eq({
-                                "A1" => Cell.new('A1'),
-                                "A2" => Cell.new('A2'),
-                                "A3" => Cell.new('A3'),
-                                "A4" => Cell.new('A4'),
-                                "B1" => Cell.new('B1'), 
-                                "B2" => Cell.new('B2'), 
-                                "B3" => Cell.new('B3'), 
-                                "B4" => Cell.new('B4'), 
-                                "C1" => Cell.new('C1'), 
-                                "C2" => Cell.new('C2'), 
-                                "C3" => Cell.new('C3'), 
-                                "C4" => Cell.new('C4'), 
-                                "D1" => Cell.new('D1'), 
-                                "D2" => Cell.new('D2'), 
-                                "D3" => Cell.new('D3'), 
-                                "D4" => Cell.new('D4')
-                                })
+  it 'has a cells that make up board' do
+    
+    expect(@board.cells.count).to eq(16)
   end
 end
