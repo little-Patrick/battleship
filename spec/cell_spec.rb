f4b590f31de23eee9e08238b5a08e8fd586a030b
Jacob Inman <jrinman6295@gmail.com>
2024-12-20 12:31:50 -0800

    render spec thought complete

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -52,7 +52,18 @@ RSpec.describe Cell do
       @cell_2.place_ship(@cruiser)
       expect(@cell_2.render).to eq('.')
 
+      expect(@cell_2.render(true)).to eq('S')
+
+      @cell_2.fire_upon
+
+      expect(@cell_2.render).to eq('H')
+      expect(@cruiser.sunk?).to eq(false)
       
+      @cruiser.hit
+      @cruiser.hit
+
+      expect(@cruiser.sunk?).to eq(true)
+      expect(@cell.render).to eq('X')
     end
   end
 end
