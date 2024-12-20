146fb305ec75ccc323714cdb9034dbd03a9ed2e3
little-Patrick <Little.patrick@outlook.com>
2024-12-20 14:54:16 -0700

    adding validating coordinates to board spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,12 +7,27 @@ RSpec.describe Board do
    
   end
 
-  it 'exists' do 
-    expect(@board).to be_an_instance_of(Board)
+  describe 'initialize' do
+    it 'exists' do 
+      expect(@board).to be_an_instance_of(Board)
+    end
+
+    it 'has a cells that make up board' do
+      expect(@board.cells.count).to eq(16)
+    end
   end
 
-  it 'has a cells that make up board' do
-    
-    expect(@board.cells.count).to eq(16)
+  describe 'validating coordinates' do
+    it 'has valid coordinate' do
+      expect(@board.valid_coordinate?('A1')).to eq(true)
+      expect(@board.valid_coordinate?('D4')).to eq(true)
+      expect(@board.valid_coordinate?('A5')).to eq(false)
+      expect(@board.valid_coordinate?('E1')).to eq(false)
+      expect(@board.valid_coordinate?('A22')).to eq(false)
+
+    end
+
   end
+
+  
 end
