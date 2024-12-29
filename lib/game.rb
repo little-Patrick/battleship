8b2381b51fd4fc25cc81b73febdbd04eb0245a99
little-Patrick <Little.patrick@outlook.com>
2024-12-29 11:25:20 -0700

    getting an outline for new classes and runner file

---
+++ lib/game.rb
@@ -0,0 +1,9 @@
+class Game 
+  attr_reader 
+
+  def initialize()
+    
+  end
+
+
+end
---
+++ lib/runner.rb
@@ -0,0 +1,8 @@
+
+
+puts "Welcome to BATTLESHIP"
+
+puts "Enter p to play. Enter q to quit."
+
+start_game = gets.chomp
+
