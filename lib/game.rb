f813a4a89517ec67ec874a8bf6e1dcff4c6b0a12
Patrick Little <little.patrick@outlook.com>
2024-12-30 11:13:42 -0700

    adding instance variables to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,35 @@
 class Game 
-  attr_reader :player_board, :computer_board
+  attr_reader :player_board, :computer_board, :player_ships, :computer_ships
+  
 
   def initialize()
     @player_board = Board.new
     @computer_board = Board.new
+    @player_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
+    @computer_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
   end
 
   def start
-    puts "Welcome to BATTLESHIP"
-    ## this will create the loop for the game
+    loop do 
+      puts "Welcome to BATTLESHIP"
+
+      computer_ships.each do 
+      
   end
 
   def game_over?
-    # when the board has no more "S" showing
+    
   end
 
 
-
-  
 end
+
+
+# place computer ships
+# 
+#
+#
+#
+# player places their ships
+# 
+#
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Game do
   describe 'initialize' do
     it 'exists' do
       expect(@game).to be_an_instance_of(Game)
+      expect(@game.player_ships).to eq([Ship.new("Cruiser", 3), Ship.new("Submarine", 2)])
     end
 
     it 'starts' do
