b8ab2f8ea5bb9531857c9bd5b07493449e32b477
Jacob Inman <jrinman6295@gmail.com>
2025-01-02 13:10:04 -0800

    game works, issue with computer placing two submarines still

--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,7 @@ class Game
 
 
   def start
-    puts "The seas be still... for now. Place your ships, captain."
+    puts "The seas be still... for now. Place your ships, Admiral."
     place_computers_ships
     place_player_ships
 
@@ -33,14 +33,14 @@ class Game
 
 
   def game_over?
-
-    player_ships_afloat = @player_board.render(true).flatten.include?("S")
-    computer_ship_afloat = @computer_board.render(true).flatten.include?("S")
-  
-    if player_ship_afloat = false
+# binding.pry
+    player_ships_afloat = @player_board.render(true).include?("S")
+    computer_ships_afloat = @computer_board.render(true).include?("S")
+  # binding.pry
+    if player_ships_afloat == false
       puts "you lose"
       true
-    elsif computer_ship_afloat = false
+    elsif computer_ships_afloat == false
       puts "you win"
       true
     else 
@@ -51,9 +51,11 @@ class Game
 
   def player_turn
     loop do
+      puts "Coordinate to fire upon?"
       player_guess = gets.chomp.upcase
       if @computer_board.valid_coordinate?(player_guess) && @computer_board.cells[player_guess].fired_upon? == false
         @computer_board.cells[player_guess].fire_upon
+        break
       else
         puts "Invalid Coordinate, Try Firing Again."
       end
@@ -65,6 +67,7 @@ class Game
       computer_guess = @player_board.cells.keys.sample
       if @player_board.valid_coordinate?(computer_guess) && @player_board.cells[computer_guess].fired_upon? == false
         @player_board.cells[computer_guess].fire_upon
+        break
       end
     end
   end
@@ -75,6 +78,7 @@ class Game
     placed = 0
     until placed == @computer_ships.count
       @computer_ships.each do |ship|
+        binding.pry
         potential_coordinates = @computer_board.cells.keys.sample(ship.length)
         check = @computer_board.valid_placement?(ship, potential_coordinates)
         if check == true
@@ -91,8 +95,9 @@ class Game
     placed = 0
     until placed == @player_ships.count
       @player_ships.each do |ship|
-        puts "Select #{ship.length} coordinates (A letter and a number, A-D, 1-4) for your #{ship.name}."
-        potential_coordinates = gets.chomp.upcase
+        puts "Select #{ship.length} coordinates (A letter and a number, A-D, 1-4) for your #{ship.name} to be stationed."
+        potential_coordinates = gets.chomp().to_s.upcase.split(", ")
+        # binding.pry
         check = @player_board.valid_placement?(ship, potential_coordinates)
         if check == true
           @player_board.place(ship, potential_coordinates)
@@ -103,24 +108,10 @@ class Game
   end
     
   def display_board
-    puts 'THE ENEMY'
+    puts '---THE ENEMY---'
     puts @computer_board.render
-    puts 'Friendly'
+    puts ' '
+    puts '---FRIENDLY---'
     puts @player_board.render(true)
   end
 end
-
-
-# @computer_ships.each do |ship|
-#   until ship.
-# end
-
-# place computer ships
-# select 3 random coordinates 
-#
-#
-#
-# player places their ships
-# 
-#
-#computer_ship.
--- lib/runner.rb
+++ lib/runner.rb
@@ -2,7 +2,7 @@ require './spec/spec_helper'
 
 
 puts "Welcome to BATTLESHIP"
-battleship = @Game.new
+battleship = Game.new
 puts "Enter p to play. Enter q to quit."
 
 start_game = gets.chomp()
@@ -14,3 +14,5 @@ if start_game == 'q'
 else start_game == 'p'
   battleship.start
 end
+
+puts "The final missile has been fired, and a still takes to the seas... The tale of what happened here today will live on in the whisper of the waves, and the heart of the sailors who carry on..."
