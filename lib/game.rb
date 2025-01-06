c8b0e555cd4be42cb60dab6c715852c2d0b2a7b6
Jacob Inman <jrinman6295@gmail.com>
2025-01-06 10:11:46 -0800

    type out method added for visual assistance when playing

--- lib/game.rb
+++ lib/game.rb
@@ -35,14 +35,15 @@ class Game
     player_ships_afloat = @player_board.render(true).include?("S")
     computer_ships_afloat = @computer_board.render(true).include?("S")
     if player_ships_afloat == false
-      puts "Wow you're bad at this game"
-      puts "Loser"
+      type_out("Wow...", 0.3) 
+      type_out("You're bad at this game.")
+      type_out("Maybe next time, Loser.")
       puts " "
       true
     elsif computer_ships_afloat == false
-      puts "The final missile has been fired, and a still takes to the seas..." 
-      puts "The tale of what happened here today will live on in the whisper of the waves, and the heart of the sailors who carry on..."
-      puts "You Have Emerged Victorious"
+      type_out("The final missile has been fired, and a still takes to the seas...", 0.01)
+      type_out("The tale of what happened here today will live on in the whisper of the waves, and the heart of the sailors who carry on...", 0.01)
+      type_out("You Have Emerged Victorious", 0.18)
       puts " "
       true
     else 
@@ -53,16 +54,16 @@ class Game
 
   def player_turn
     loop do
-      puts "Coordinate to fire upon?"
+      type_out("Coordinate to fire upon?", 0.01)
       player_guess = gets.chomp.upcase
       if @computer_board.valid_coordinate?(player_guess) && @computer_board.cells[player_guess].fired_upon? == false
         @computer_board.cells[player_guess].fire_upon
         if  @computer_board.cells[player_guess].empty?
           puts ' '
-          puts 'SpLoOoOoOOsH...'
+          type_out('SpLoOoOoOsH...')
         else
           puts ' '
-          puts'!!!!KAhbOooOOoOMmmM!!!'
+          type_out('!!!!KAhbOooOOoOMmmM!!!', 0.05)
         end
         break
       else
@@ -77,7 +78,7 @@ class Game
       if @player_board.valid_coordinate?(computer_guess) && @player_board.cells[computer_guess].fired_upon? == false
         @player_board.cells[computer_guess].fire_upon
         puts ' '
-        puts "----THE ENEMY FIRED UPON #{computer_guess}------"
+        type_out("THE ENEMY FIRED UPON #{computer_guess}", 0.08)
         break
       end
     end
@@ -101,7 +102,7 @@ class Game
   def place_player_ships
     @player_ships.each do |ship|
       loop do
-        puts "Select #{ship.length} coordinates (A letter and a number, A-D, 1-4. Please input in alphabetical order.) for your #{ship.name} to be stationed."
+        type_out("Select #{ship.length} coordinates (A letter and a number, A-D, 1-4. Please input in alphabetical order.) for your #{ship.name} to be stationed.", 0.01)
         potential_coordinates = gets.chomp().to_s.upcase.split(", ")
         if @player_board.valid_placement?(ship, potential_coordinates)
           @player_board.place(ship, potential_coordinates)
@@ -112,11 +113,20 @@ class Game
   end
     
   def display_board
-    puts ' '
+    type_out('      ')
     puts '---THE ENEMY---'
     puts @computer_board.render
     puts ' '
     puts '---FRIENDLY---'
     puts @player_board.render(true)
+  end
+
+  def type_out(text, delay = 0.1)
+    text.each_char do |char|
+      print char
+      $stdout.flush
+      sleep(delay)
+    end
+    puts
   end
 end
